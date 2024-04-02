50b13ef919080a507864c9a226930323e5e4696c
eqqmayo <eqqmayo@gmail.com>
2024-04-02 16:10:53 +0900

    first commit

---
+++ MyGithub/MyGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGithub
+//
+//  Created by CaliaPark on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
