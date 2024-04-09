d401ee0bd2af3307463883c5bf0d7140f0d0b232
eqqmayo <eqqmayo@gmail.com>
2024-04-09 15:08:36 +0900

    API 파일 추가

---
+++ MyGithub/MyGithub/Models/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  MyGithub
+//
+//  Created by CaliaPark on 4/9/24.
+//
+
+import Foundation
+
+struct API {
+    static let baseURL = "https://api.github.com/"
+    static let token = "Bearer YOUR_TOKEN"
+}
